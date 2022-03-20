5091527d6aafeb63b57a947bf8e1b395e5bba231
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 18:45:26 -0400

    feat: check email domain parts length

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -8,11 +8,14 @@ export class Email {
 
     const [localPart, domain] = email.split('@');
 
-    // check if email local part
+    // check email local part
     if (!localPart || localPart.length > 64) return false;
 
-    // check if email domain
+    // check email domain
     if (!domain || domain.length > 255) return false;
+
+    const domainParts = domain.split('.');
+    if (domainParts.some((part) => part.length > 63)) return false;
 
     // check if email length is smaller or equal than 320 chars
     if (email.length > 320) return false;
