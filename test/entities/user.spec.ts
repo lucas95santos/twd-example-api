c718e1db4026037e3d020305512cc588be492817
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 19:34:56 -0400

    test: create a user

--- test/entities/user.spec.ts
+++ test/entities/user.spec.ts
@@ -1,12 +1,14 @@
-import { UserData, User } from '../../src/entities';
+import { left } from '../../src/shared';
+import { UserData, User, InvalidEmailError } from '../../src/entities';
 
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
     const newUser: UserData = {
-      email: 'indalid@mail.com',
+      email: 'invalid_mail',
       name: 'any_name',
     };
 
     const error = User.create(newUser);
+    expect(error).toEqual(left(new InvalidEmailError()));
   });
 });
