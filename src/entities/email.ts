60ce9aa81bc54333f27941852f85f72b47fb2c64
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 18:16:52 -0400

    feat: check if email contains @ char

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,11 @@
 export class Email {
   static validate(email: string): boolean {
+    // check if email is empty
     if (!email) return false;
+
+    // check if email contain @ char
+    if (!email.includes('@')) return false;
+
     return true;
   }
 }
