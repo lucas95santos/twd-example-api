7d777ad684670cc99a674bfa8c34a28071a1798a
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 18:17:52 -0400

    feat: check if email local part is smaller than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,6 +6,11 @@ export class Email {
     // check if email contain @ char
     if (!email.includes('@')) return false;
 
+    const [localPart] = email.split('@');
+
+    // check if email local part is smaller than 64 chars
+    if (localPart.length > 64) return false;
+
     return true;
   }
 }
