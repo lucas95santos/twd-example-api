aee74aa989aa1bc0eb131044207608c7b39a3b34
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 19:34:50 -0400

    feat: create a user

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,7 +1,24 @@
+import { Either, right, left } from '../shared';
+import { InvalidEmailError } from './errors';
+
 const EMAIL_FORMAT =
   /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
 export class Email {
+  public readonly value: string;
+
+  private constructor(email: string) {
+    this.value = email;
+  }
+
+  public static create(email: string): Either<InvalidEmailError, Email> {
+    if (Email.validate(email)) {
+      return right(new Email(email));
+    }
+
+    return left(new InvalidEmailError(email));
+  }
+
   static validate(email: string): boolean {
     // check if email is empty
     if (!email) return false;
--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,5 +1,12 @@
-import { UserData } from 'entities';
+import { UserData, InvalidEmailError, Email } from './';
+import { Either, left, right } from '../shared';
 
 export class User {
-  static create(user: UserData) {}
+  static create(user: UserData): Either<InvalidEmailError, UserData> {
+    const emailOrError = Email.create(user.email);
+
+    if (emailOrError.isLeft()) return left(new InvalidEmailError());
+
+    return right(user);
+  }
 }
