a5b2adcc29b52a19e7ae3e7a119d0d3acc1136e6
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 19:50:19 -0400

    feat: create a name validator

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,8 +1,14 @@
-import { UserData, InvalidEmailError, Email } from './';
+import { UserData, InvalidEmailError, InvalidNameError, Email, Name } from './';
 import { Either, left, right } from '../shared';
 
 export class User {
-  static create(user: UserData): Either<InvalidEmailError, UserData> {
+  static create(
+    user: UserData,
+  ): Either<InvalidEmailError | InvalidNameError, UserData> {
+    const nameOrError = Name.create(user.name);
+
+    if (nameOrError.isLeft()) return left(nameOrError.value);
+
     const emailOrError = Email.create(user.email);
 
     if (emailOrError.isLeft()) return left(emailOrError.value);
