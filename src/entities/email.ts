a92ecefcf95a450d15d2ab346d58f0da2387ee9e
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 18:33:35 -0400

    feat: check if domain length is smaller than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,10 +6,13 @@ export class Email {
     // check if email contain @ char
     if (!email.includes('@')) return false;
 
-    const [localPart] = email.split('@');
+    const [localPart, domain] = email.split('@');
 
     // check if email local part is smaller or equal than 64 chars
     if (localPart.length > 64) return false;
+
+    // check if email domain is smaller or equal than 255 chars
+    if (domain.length > 255) return false;
 
     // check if email length is smaller or equal than 320 chars
     if (email.length > 320) return false;
