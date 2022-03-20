7b3e88bd41961a5f98698a561e1fc1ecd9b2411a
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 17:43:19 -0400

    test: testing in memory user repository

---
+++ test/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -0,0 +1,38 @@
+import { UserData } from '../../../../src/usecases/register-user-on-mailing-list/ports';
+import { InMemoryUserRepository } from '../../../../src/usecases/register-user-on-mailing-list/repository';
+
+describe('In memory User Repository', () => {
+  test('should return null if user is not found', async () => {
+    const users: UserData[] = [];
+    const userRepository = new InMemoryUserRepository(users);
+
+    const user = await userRepository.findUserByEmail('any@mail.com');
+    expect(user).toBeNull();
+  });
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = [];
+    const userRepository = new InMemoryUserRepository(users);
+
+    const newUser: UserData = {
+      name: 'any_name',
+      email: 'any@mail.com',
+    };
+
+    await userRepository.add(newUser);
+
+    const user = await userRepository.findUserByEmail('any@mail.com');
+    expect(user?.name).toBe(newUser.name);
+  });
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [
+      { name: 'any_name', email: 'any@mail.com' },
+      { name: 'second_name', email: 'second@mail.com' },
+    ];
+
+    const userRepository = new InMemoryUserRepository(users);
+    const returnedUsers = await userRepository.findAllUsers();
+    expect(returnedUsers.length).toBe(2);
+  });
+});
