e8116ff47e19e5050491e9d3d6b6745b6400e849
Lucas Santos <lucasa95santos@gmail.com>
2022-03-20 21:35:25 -0400

    test: register valid user on mailing list

--- test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,3 +1,4 @@
+import { RegisterUserOnMailingList } from '../../../src/usecases/register-user-on-mailing-list/register-user-on-mailing-list';
 import { UserData } from '../../../src/entities';
 import { UserRepository } from '../../../src/usecases/register-user-on-mailing-list/ports';
 import { InMemoryUserRepository } from '../../../src/usecases/register-user-on-mailing-list/repository';
@@ -6,16 +7,18 @@ describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = [];
     const repository: UserRepository = new InMemoryUserRepository(users);
-    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
-    //   repository,
-    // );
-    // const newUser: UserData = {
-    //   name: 'any_name',
-    //   email: 'any@email.com',
-    // };
+    const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+      repository,
+    );
+    const newUser: UserData = {
+      name: 'any_name',
+      email: 'any@email.com',
+    };
 
-    // const usecaseResponse = await usecase.execute(newUser);
-    // const user = await repository.findUserByEmail(newUser.email);
-    expect(1).toBe(1);
+    const usecaseResponse = await usecase.execute(newUser);
+    const user = await repository.findUserByEmail(newUser.email);
+
+    expect(usecaseResponse.value.name).toBe(newUser.name);
+    expect(user?.name).toBe(newUser.name);
   });
 });
